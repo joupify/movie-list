8c33e2d977730d1087b782d264b9eafd4519cb24
mj <malika.housni@gmail.com>
2023-06-25 19:06:09 +0200

    image set with cloudinary installed and rails c  ok

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -2,5 +2,5 @@ class Movie < ApplicationRecord
   has_many :bookmarks, dependent: :destroy
   has_many :lists, through: :bookmarks
   validates :title, :overview, uniqueness: true, presence: true
-  has_one_attached :poster
+
 end
